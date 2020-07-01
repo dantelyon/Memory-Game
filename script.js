6cf4de953d2747ae227cdcd999753b5862829b64
dantelyon <dightmisc@gmail.com>
2020-07-01 11:25:30 +0200

    time to finish this project.

--- script.js
+++ script.js
@@ -1,10 +1,64 @@
 
+let buttons = document.getElementById("buttons")
+let selected = document.querySelectorAll(".active a")[0];
+buttons.onclick = function(event){
+	let target = event.target;
+	if (selected) {selected.parentNode.classList.remove("active")};
+	if (target.tagName == "A") {
+		selected = target
+		target.parentNode.classList.add("active")
+	}
+}
 
+var btnContainer = document.getElementById("buttons");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+} 
 
+function doIt(type='portrait'){
+  var something = Object.keys(images[type])
+  var something2 = shuffle(something)
+  var something3 = something2.slice(0,8)
+  var something4 = something3.concat(something3)
+  var something5 = shuffle(something4)
+  return something5
+}
 
+window.onload = function(){
+  for (let i = 0; i < 16; i++) {
+    const card = document.createElement('li');
+    const front = document.createElement('div');
+    const back = document.createElement('div');
+    card.classList.add('portrait');
+    front.classList.add('front');
+    back.classList.add('back');
+    let temp = doIt()
+    back.style.backgroundImage = `url('portrait/${temp[i]}.jpg')`;
+    card.dataset.name = temp[i]
+    document.getElementById('game').appendChild(card);
+    card.appendChild(front);
+    card.appendChild(back);
+  }
+};
 
-const imgArray = ['img/point.png', 'img/contemporary.png', 'img/modern.png', 'img/cubism.png', 'img/realism.png', 'img/postimp.png', 'img/soleil.png', 'img/express.png',]
+const changeCards = (type) => {
+  resetGuesses()
+  document.querySelectorAll('#game > LI').forEach(card => card.className = type)
+  let counter = 0
+  let temp = doIt(type)
+  document.querySelectorAll(".back").forEach(card => {card.style.backgroundImage = `url('${type}/${temp[counter]}.jpg')`; counter++} )
+  counter = 0
+}
 
+document.getElementById('buttons').onclick = (e) => {
+  if (e.target.nodeName == 'A') {changeCards(e.target.id)} //A used to be BUTTON
+}
+ 
 function shuffle(a) {
     for (let i = a.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
@@ -12,22 +66,6 @@ function shuffle(a) {
     }
     return a;
 }
-
-const gameGrid = shuffle(imgArray.concat(imgArray))
-
-gameGrid.forEach(item => {
-  const card = document.createElement('li');
-  const front = document.createElement('div');
-  const back = document.createElement('div');
-  card.classList.add('portrait');
-  front.classList.add('front');
-  back.classList.add('back');
-  back.style.backgroundImage = `url(${item})`;
-  document.getElementById('game').appendChild(card);
-  card.appendChild(front);
-  card.appendChild(back);
-});
-
 
 let firstGuess = '';
 let secondGuess = '';
@@ -58,15 +96,10 @@ document.getElementById('game').addEventListener('click', event => {
       secondGuess = event.target.nextSibling.style.backgroundImage
       event.target.parentNode.classList.toggle('selected')}
     if (firstGuess === secondGuess) {
-      setTimeout(i => document.querySelectorAll('.selected').forEach(card => {card.classList.add('match');}), 1200)
+      setTimeout(i => document.querySelectorAll('.selected').forEach(card => {card.classList.add('match'); document.querySelector("P").innerHTML = card.className}), 1200)
     }
-    if (firstGuess && secondGuess) {
-      setTimeout(resetGuesses, 1200)}
+    if (firstGuess && secondGuess) {setTimeout(resetGuesses, 1200)}
     previousTarget = event.target;
   }
 });
 
-document.getElementById('Portrait').addEventListener('click', i => document.querySelectorAll('LI').forEach(card =>card.className = "portrait")) 
-document.getElementById('Surrealism').addEventListener('click', i => document.querySelectorAll('LI').forEach(card => card.className = "surrealism"))
-document.getElementById('Contemporary').addEventListener('click', i => document.querySelectorAll('LI').forEach(card => card.className = "contemporary"))
- 
