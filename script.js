f81608f9fc890e75db133ebb77d26e22aedaa469
dantelyon <dightmisc@gmail.com>
2020-07-02 14:25:45 +0200

    refactored a lot of my old code.

--- script.js
+++ script.js
@@ -1,64 +1,21 @@
 
-let buttons = document.getElementById("buttons")
-let selected = document.querySelectorAll(".active a")[0];
-buttons.onclick = function(event){
-	let target = event.target;
-	if (selected) {selected.parentNode.classList.remove("active")};
-	if (target.tagName == "A") {
-		selected = target
-		target.parentNode.classList.add("active")
-	}
-}
 
-var btnContainer = document.getElementById("buttons");
-var btns = btnContainer.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
-} 
-
-function doIt(type='portrait'){
-  var something = Object.keys(images[type])
-  var something2 = shuffle(something)
-  var something3 = something2.slice(0,8)
-  var something4 = something3.concat(something3)
-  var something5 = shuffle(something4)
-  return something5
-}
-
-window.onload = function(){
-  for (let i = 0; i < 16; i++) {
-    const card = document.createElement('li');
-    const front = document.createElement('div');
-    const back = document.createElement('div');
-    card.classList.add('portrait');
-    front.classList.add('front');
-    back.classList.add('back');
-    let temp = doIt()
-    back.style.backgroundImage = `url('portrait/${temp[i]}.jpg')`;
-    card.dataset.name = temp[i]
-    document.getElementById('game').appendChild(card);
-    card.appendChild(front);
-    card.appendChild(back);
-  }
-};
-
-const changeCards = (type) => {
+function changeCards(type) {
   resetGuesses()
   document.querySelectorAll('#game > LI').forEach(card => card.className = type)
-  let counter = 0
-  let temp = doIt(type)
-  document.querySelectorAll(".back").forEach(card => {card.style.backgroundImage = `url('${type}/${temp[counter]}.jpg')`; counter++} )
-  counter = 0
+  let something = shuffle(Object.keys(images[type])).slice(0,8)
+  let temp = shuffle(something.concat(something))
+  document.querySelectorAll(".back").forEach((card, i) => {card.style.backgroundImage = `url('${type}/${temp[i]}.jpg')`} )
 }
 
-document.getElementById('buttons').onclick = (e) => {
-  if (e.target.nodeName == 'A') {changeCards(e.target.id)} //A used to be BUTTON
-}
- 
+function resetGuesses() {
+  firstGuess = '';
+  secondGuess = '';
+  guesses = 0;
+  previousTarget = null;
+  document.querySelectorAll('.selected').forEach(card => {card.classList.toggle('selected')});
+};
+
 function shuffle(a) {
     for (let i = a.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
@@ -67,18 +24,40 @@ function shuffle(a) {
     return a;
 }
 
+
+/* BUTTONS HEADER*/
+var btnContainer = document.getElementById("buttons");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+btnContainer.addEventListener('click', (e) => e.target.nodeName=='A' && changeCards(e.target.id))
+
+
+window.onload = function(){
+  for (let i = 0; i < 16; i++) {
+    let card = document.createElement('li');
+    let front = document.createElement('div');
+    let back = document.createElement('div');
+    card.classList.add('portrait');
+    front.classList.add('front');
+    back.classList.add('back');
+    card.appendChild(front);
+    card.appendChild(back);
+    document.getElementById('game').appendChild(card);
+  }
+  changeCards("portrait")
+};
+
+
 let firstGuess = '';
 let secondGuess = '';
-let count = 0;
+let guesses = 0;
 let previousTarget = null;
-
-const resetGuesses = () => {
-  firstGuess = '';
-  secondGuess = '';
-  count = 0;
-  previousTarget = null;
-  document.querySelectorAll('.selected').forEach(card => {card.classList.toggle('selected')});
-};
 
 document.getElementById('game').addEventListener('click', event => {
   if ( event.target.nodeName === 'UL' ||
@@ -87,19 +66,20 @@ document.getElementById('game').addEventListener('click', event => {
        event.target.parentNode.classList.contains('match') ) 
     return
 
-  if (count < 2) {
-    count++;
-    if (count === 1) {
-      firstGuess = event.target.nextSibling.style.backgroundImage
-      event.target.parentNode.classList.toggle('selected')}
-      else {
-      secondGuess = event.target.nextSibling.style.backgroundImage
-      event.target.parentNode.classList.toggle('selected')}
-    if (firstGuess === secondGuess) {
-      setTimeout(i => document.querySelectorAll('.selected').forEach(card => {card.classList.add('match'); document.querySelector("P").innerHTML = card.className}), 1200)
-    }
-    if (firstGuess && secondGuess) {setTimeout(resetGuesses, 1200)}
-    previousTarget = event.target;
-  }
+	guesses++
+	if (guesses === 1) {
+		firstGuess = event.target.nextSibling.style.backgroundImage;
+		event.target.parentNode.classList.toggle('selected');
+	}
+	else if (guesses === 2) {
+		secondGuess = event.target.nextSibling.style.backgroundImage;
+		event.target.parentNode.classList.toggle('selected');
+		if (firstGuess === secondGuess) {
+			setTimeout(function() {document.querySelectorAll('.selected').forEach(card => card.classList.add('match'))}, 1200)
+		};
+		if (firstGuess && secondGuess) {setTimeout(resetGuesses, 1200)}
+	};
+	previousTarget = event.target;
 });
+
 
